31f99729539580e4256e64cb6ca5f7d3e60f2a88
NazreenShanavaz <nazreenshanavaz2004@gmail.com>
2025-01-20 20:39:15 +0530

    binary_decimal

---
+++ binarytodecimal.py
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+binum = input("Enter binary number:")
+decnum = int(binum,2)
+=======
+binum = input("Enter binary number:")
+decnum = int(binum,2)
+>>>>>>> 01fd0cec49479ce881485b7b54227e488d81a816
+print("The decimal number is:",decnum)
